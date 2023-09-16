c51642fa50cccd89eb97c1b9c5f3d950792cbd6e
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-09-16 21:45:41 +0700

    bottom navigation bar

--- lib/mainScreen/main_screen.dart
+++ lib/mainScreen/main_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
@@ -8,11 +7,31 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin{
+  TabController? _tabController;
+
+  int selectedIndex = 0;
+
+  onItemClicked(int index){
+    setState(() {
+      selectedIndex=index;
+      _tabController!.index=selectedIndex;
+    });
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _tabController =TabController(length: 4, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      title:const Text('Welcome'),
-    ),);
+    return Scaffold(
+     body:TabBarView(children: [], physics: NeverScrollableScrollPhysics(),)
+    );
   }
 }
