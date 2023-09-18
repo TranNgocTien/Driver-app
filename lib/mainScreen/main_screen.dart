924f12fac20f2fea76d0eb39081fe6d89aef2a54
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-09-18 11:13:32 +0700

    bottom navigation bar

--- lib/mainScreen/main_screen.dart
+++ lib/mainScreen/main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:driver_app/tabs/earning_tab.dart';
+import 'package:driver_app/tabs/home_tab.dart';
+import 'package:driver_app/tabs/profile_tab.dart';
+import 'package:driver_app/tabs/rating_tab.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -7,31 +11,70 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin{
+class _MainScreenState extends State<MainScreen>
+    with SingleTickerProviderStateMixin {
   TabController? _tabController;
 
   int selectedIndex = 0;
 
-  onItemClicked(int index){
+  onItemClicked(int index) {
     setState(() {
-      selectedIndex=index;
-      _tabController!.index=selectedIndex;
+      selectedIndex = index;
+      _tabController!.index = selectedIndex;
     });
   }
-
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-    _tabController =TabController(length: 4, vsync: this);
+    _tabController = TabController(length: 4, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body:TabBarView(children: [], physics: NeverScrollableScrollPhysics(),)
+      body: TabBarView(
+        physics:const NeverScrollableScrollPhysics(),
+        controller: _tabController,
+        children: const [
+          EarningTab(),
+          HomeTab(),
+          ProfileTab(),
+          RatingsTab(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items:const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label:"Họme",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label:"Earning",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label:"Ratings"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label:"Account",
+          ),
+        ],
+
+        unselectedItemColor:Colors.white54,
+        selectedItemColor:Colors.white,
+        backgroundColor:Colors.black,
+        type:BottomNavigationBarType.fixed,
+        selectedLabelStyle:const TextStyle(fontSize: 14,),
+        showUnselectedLabels: true,
+        currentIndex: selectedIndex,
+        onTap:onItemClicked,
+
+      ),
     );
   }
 }
---
+++ lib/tabs/earning_tab.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class EarningTab extends StatefulWidget {
+  const EarningTab({super.key});
+
+  @override
+  State<EarningTab> createState() => _EarningTabState();
+}
+
+class _EarningTabState extends State<EarningTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:Text(
+        'Earning tab',
+      ),
+    );
+  }
+}
